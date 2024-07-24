113969c23d0154163b4338f3327b9c3e793817ff
dushan <dushan4@gmail.com>
2024-07-24 16:50:00 +1000

    Added Vocab class for encoding/decoding tokens into numerical matrices.

---
+++ w2v_numpy.py
@@ -0,0 +1,73 @@
+from typing import Sequence, Tuple, List
+import numpy.typing as npt
+
+import numpy as np
+
+
+
+class Vocab:
+    """Generates vocabulary and encoding/decoding utils from raw train_data.
+
+    For encoding/decoding methods, assumes input dims are 3 long. E.g.
+    [Vocab.size, embed_dim, batch_size]. Include batch dim but set to 1 if not
+    batching.
+    """
+
+    @property
+    def vocab(self):
+        return self._vocab
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def index(self):
+        return self._index
+
+
+    @property
+    def token2index(self):
+        return self._token2index
+
+    def __init__(self, data: Sequence[Sequence[str]]):
+        vocab = set(' '.join([' '.join(line) for line in data]).split(' '))
+
+        self._vocab = np.expand_dims(np.array(list(vocab)), 1)  # dims: [V, 1]
+        self._vocab.sort()
+        self._size = len(self._vocab)
+        self._index = np.expand_dims(np.arange(self._size), 1)  # dims: [V, 1]
+        self._token2index = dict(zip(self._vocab.squeeze(),
+                                     self._index.squeeze()))
+
+
+    def encode_ohe(self, tokens: npt.NDArray[str]):
+        """Encodes from tokens to flat OHE vector.
+
+        Input dims: [Context length - 1, Batch]
+        """
+        # If a word is not in the vocab, it gets ignored.
+        token_mat = np.zeros((self.size, tokens.shape[1]))
+        for i, batch in enumerate(tokens.T):
+            # we have to generate ohe for each word and add them. If a word occurs twice within the context they will
+            # get a value of 2 in there position of the OHE vector. This means it will get double the weight in the
+            # projection layer, which has the effect of averaging all words, including duplicate instances in the
+            # projection layer.
+            for word in batch:
+                token_mat[:, i] += np.isin(self.vocab, word).astype('float').squeeze()
+        return token_mat  # dims: [vocab.size, batch_size]
+
+
+    def encode_idx(self, tokens: npt.NDArray[str]):
+        """From text tokens to index numbers.
+
+        Input dims: [Context length - 1, Batch]
+        """
+        # If a word is not in the vocab, it gets ignored.
+        # Need to iterate through tokens to handle repeated words within
+        # a batch.
+        index_mat = np.zeros(tokens.shape, dtype=self._index.dtype)
+        for i, batch in enumerate(tokens.T):
+            index_mat[:, i] = np.concatenate([self._index[self.vocab == word] for word in batch])
+        return index_mat  # dims -> [window-1, batch_size]
+
